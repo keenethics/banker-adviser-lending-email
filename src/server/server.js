8a98f20d67f1a4ffe5be1932551da7bf806a5ce4
stefan9541 <stepi08@rambler.ru>
2020-03-21 12:17:49 +0200

    cors

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,15 @@ app.listen(PORT, () => console.log("========= SERVER IS RUNNING AT " + PORT));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./../../build")));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./../../build", "index.html"));
 });
