da13f5ff63747b00b183baa380efd0833b974465
stefan9541 <stepi08@rambler.ru>
2020-07-09 15:09:41 +0300

    add sentry for node, refactor server folder structure

--- src/server/utils/nodemailer-transport.js
+++ src/server/utils/nodemailer-transport.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
+const Sentry = require("@sentry/node");
 
 const transport = {
   host: "smtp.gmail.com", // e.g. smtp.gmail.com
   auth: {
-    user: process.env.USER,
-    pass: process.env.PASSWORD,
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_EMAIL_PASSWORD,
   },
   tls: {
     rejectUnauthorized: false,
@@ -14,7 +15,7 @@ const transport = {
 const transporter = nodemailer.createTransport(transport);
 transporter.verify((error) => {
   if (error) {
-    console.log(error);
+    Sentry.captureException(error);
   } else {
     console.log("All works fine, congratz!");
   }
---
+++ src/server/utils/sentry-init.js
@@ -0,0 +1,6 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn:
+    "https://7e0f74d3bcbd450793ab8ae6a3839b6d@o418172.ingest.sentry.io/5320377",
+});
